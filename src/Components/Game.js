62c0886ee6a92ff356ebd7c09c472cbc742db06e
wmaKRET <wkret91.2@gmail.com>
2022-07-02 16:04:39 +0200

    added timer states, added input state, added start and end game functions

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react"
 import Team from "./Team"
 
 function Game(props) {
+    const TIMER_VALUE = 5
+
     const [teamsArray, setTeamsArray] = useState([])
+    const [inputText, setInputText] = useState('')
+    const [timeRemaining, setTimeRemaining] = useState(TIMER_VALUE)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
 
     const getData = () => {
         fetch('../data.json')
@@ -11,9 +16,33 @@ function Game(props) {
             .catch(error => console.log(`Error: ${error}`))
     }
 
+    function handleChange(event){
+        const {value} = event.target
+        setInputText(value)
+        console.log(inputText)
+    }
+
+    function startGame(){
+        setTimeRemaining(TIMER_VALUE)
+        setIsTimeRunning(true)
+        setInputText('')
+    }
+
+    function endGame(){
+        setIsTimeRunning(false)
+    }
+
     useEffect(() => {
         getData()
     }, [])
+
+    useEffect(() => {
+         if (isTimeRunning && timeRemaining > 0){
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+         } else if (timeRemaining === 0) endGame()
+    }, [timeRemaining, isTimeRunning])
 
     const teamElements = teamsArray.map(team => (
         <Team 
@@ -26,14 +55,23 @@ function Game(props) {
 
     return (
         <main className="game">
+            <div className="game__hud">
+                <p>Time Remaining: {timeRemaining}</p>
+                <p>0/20 Guessed</p>
+            </div>
             <div className="game__panel">
                 <button
                     className="game__panel-btn"
+                    onClick={startGame}
+                    disabled={isTimeRunning}
                 >START
                 </button>
                 <input
+                    value={inputText}
                     className="game__panel-input"
                     placeholder="Type here..."
+                    onChange={handleChange}
+                    disabled={!isTimeRunning}
                 />
             </div>
             <div className="game__team-list">
