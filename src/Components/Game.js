9c51f7cc686b11536690221757cf841ef7da0d8d
wmaKRET <wkret91.2@gmail.com>
2022-07-04 06:28:18 +0200

    updated fetch link

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -14,7 +14,7 @@ function Game(props) {
     const inputRef = useRef(null)
 
     const getData = () => {
-        fetch('../data.json')
+        fetch('http://wmakret.github.io/name-football-teams/public/users.json')
             .then(response => response.json())
             .then(data => setTeamsArray(data[props.league].teams))
             .catch(error => console.log(`Error: ${error}`))
